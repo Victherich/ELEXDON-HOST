5c4e03d510a4e18ad50747ba1ceadceb083f999d
Victor <victherich@gmail.com>
2026-05-20 14:07:15 +0100

    hp

--- src/components/DomainSearch.jsx
+++ src/components/DomainSearch.jsx
@@ -431,7 +431,7 @@ const handleSubmit = async (e) => {
         </Form>
 
         <Title style={{ fontSize: "1rem" }}>
-          .com ₦28,000│.com.ng ₦10,500│.ng ₦17,500│.org ₦30,000│.net 40000│ .tech ₦93,000 |
+          .com ₦28,500│.com.ng ₦13,500│.ng ₦17,500│.org ₦30,000│.net 40000│ .tech ₦120,000 |
         </Title>
 
         {result && (
